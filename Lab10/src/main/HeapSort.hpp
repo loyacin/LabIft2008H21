f3472239ad5d488cd1525fd69318c0efabc588a3
Loic Ricaud <loric58@ulaval.ca>
2021-03-26 12:28:29 -0400

    Lab10: fin du code.

--- Lab10/src/main/HeapSort.hpp
+++ Lab10/src/main/HeapSort.hpp
@@ -1,4 +1,80 @@
 namespace heapSort
 {
 
+    size_t leftChild(size_t parent)
+    {
+        return 2 * parent + 1;
+    }
+
+    template<typename E>
+    size_t getGreatestChildPosition(const std::vector<E> &elements, size_t parentPosition, size_t elementsSize)
+    {
+        size_t leftChildPosition = leftChild(parentPosition);
+        if (leftChildPosition < elementsSize - 1
+            && elements[leftChildPosition] < elements[leftChildPosition + 1])
+        {
+            leftChildPosition++;
+        }
+        return leftChildPosition;
+    }
+
+    template <typename  E>
+    void percDown(std::vector<E> & elements, size_t parentPosition, size_t elementsSize)
+    {
+        while (leftChild(parentPosition) < elementsSize)
+        {
+            size_t childPosition = getGreatestChildPosition(elements, parentPosition, elementsSize);
+            if (elements[parentPosition] < elements[childPosition])
+            {
+                std::swap(elements[parentPosition], elements[childPosition]);
+                parentPosition = childPosition;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    template <typename E>
+    std::vector<E> constructHeap(const std::vector<E> & elements)
+    {
+        std::vector<E> heap(elements);
+        if (!heap.empty())
+        {
+            size_t heapSize = heap.size();
+            size_t lastParent = heapSize / 2 - 1;
+            for (size_t i = lastParent; ; i--)
+            {
+                percDown(heap, i, heapSize);
+                if (i == 0)
+                {
+                    break;
+                }
+            }
+        }
+        return heap;
+    }
+
+    template <typename E>
+    void _heapSort(std::vector<E> & elementsToSort)
+    {
+        if (elementsToSort.empty())
+        {
+            return;
+        }
+        for (size_t i = elementsToSort.size() - 1; i > 0; i--)
+        {
+            std::swap(elementsToSort[0], elementsToSort[i]); // propriété tas: racine est le plus grand élément.
+            percDown(elementsToSort, 0, i); // reconstruire un monceau en ignorant l'ancien plus grand élément.
+        }
+    }
+
+    template <typename E>
+    std::vector<E> heapSort(const std::vector<E> & elementsToSort)
+    {
+        std::vector<E> heapToSort = constructHeap(elementsToSort);
+        _heapSort(heapToSort);
+        return heapToSort;
+    }
 }
--- Lab10/src/test/HeapSortTest.cpp
+++ Lab10/src/test/HeapSortTest.cpp
@@ -2,8 +2,40 @@
 #include "../main/HeapSort.hpp"
 #include <iostream>
 
-using namespace heapSort;
-
-TEST(HeapSort, givenAnEmptyVector_whenConstructHeap_thenHeadIsEmpty)
+TEST(HeapSort, givenAnEmptyVector_whenConstructHeap_thenHeapIsEmpty)
 {
+    std::vector<int> emptyVector;
+
+    std::vector<int> returnedHeap = heapSort::constructHeap(emptyVector);
+
+    ASSERT_TRUE(returnedHeap.empty());
+}
+
+TEST(HeapSort, givenANonEmptyVector_whenConstructHeap_thenGoodHeapIsReturned)
+{
+    std::vector<int> nonEmptyVector{2, 9, 7, 8, 5, 8};
+
+    std::vector<int> returnedHeap = heapSort::constructHeap(nonEmptyVector);
+
+    std::vector<int> expectedHeap{9, 6, 8, 2, 5, 7};
+    ASSERT_EQ(expectedHeap, returnedHeap);
+}
+
+TEST(HeapSort, givenAnEmptyVector_whenHeapSort_thenSortedHeapIsEmpty)
+{
+    std::vector<int> emptyVector;
+
+    std::vector<int> returnedSortedHeap = heapSort::heapSort(emptyVector);
+
+    ASSERT_TRUE(returnedSortedHeap.empty());
+}
+
+TEST(HeapSort, givenANonEmptyVector_whenConstructHeap_thenGoodSortedHeapIsReturned)
+{
+    std::vector<int> nonEmptyVector{94, 67, 18, 44, 55, 12, 6, 42};
+
+    std::vector<int> returnedHeap = heapSort::heapSort(nonEmptyVector);
+
+    std::vector<int> expectedHeap{6, 12, 18, 42, 44, 55, 67, 94};
+    ASSERT_EQ(expectedHeap, returnedHeap);
 }
