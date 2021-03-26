8cfeeef071d282f2c87f5f827149fce0c42e78a3
Loic Ricaud <loric58@ulaval.ca>
2021-03-26 11:22:51 -0400

    Lab10: code de base.

---
+++ Lab10/src/test/HeapSortTest.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "../main/HeapSort.hpp"
+#include <iostream>
+
+using namespace heapSort;
+
+TEST(HeapSort, givenAnEmptyVector_whenConstructHeap_thenHeadIsEmpty)
+{
+}
