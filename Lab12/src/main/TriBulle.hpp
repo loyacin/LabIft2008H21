f62131b493258d1f44af4928f9a3ec1a6218aa2a
Loic Ricaud <loric58@ulaval.ca>
2021-04-23 12:28:15 -0400

    Lab13: fin des tris.

--- Lab12/src/main/TriBulle.hpp
+++ Lab12/src/main/TriBulle.hpp
@@ -1,10 +1,55 @@
 #include <vector>
+#include <cstddef>
 
 namespace labTris {
 
-template<typename E>
-void triBulle(std::vector<E> & p_v) {
-	
-}
+    template<typename E>
+    bool shakeStartEnd(const std::vector<E> &p_v);
+
+    template<typename E>
+    bool shakeEndStart(const std::vector<E> &p_v);
+
+    template<typename E>
+    void triBulle(std::vector<E> & p_v) {
+        bool swapped;
+        do
+        {
+            swapped = shakeStartEnd(p_v);
+            if (!swapped) {
+                break;
+            }
+            swapped = shakeEndStart(p_v);
+        } while (swapped);
+    }
+
+    template<typename E>
+    bool shakeEndStart(const std::vector<E> &p_v)
+    {
+        bool swapped = false;
+        for (size_t i = p_v.size() - 2; i >= 0; i--)
+        {
+            if (p_v[i] > p_v[i + 1])
+            {
+                std::swap(p_v[i], p_v[i + 1]);
+                swapped = true;
+            }
+        }
+        return swapped;
+    }
+
+    template<typename E>
+    bool shakeStartEnd(const std::vector<E> &p_v)
+    {
+        bool swapped = false;
+        for (size_t i = 0; i < p_v.size() - 2; i++)
+        {
+            if (p_v[i] > p_v[i + 1])
+            {
+                std::swap(p_v[i], p_v[i + 1]);
+                swapped = true;
+            }
+        }
+        return swapped;
+    }
 
 } // Fin namespace
--- Lab12/src/main/TriShell.hpp
+++ Lab12/src/main/TriShell.hpp
@@ -14,9 +14,32 @@
 
 namespace labTris {
 
-template<typename E>
-void triShell(std::vector<E> & p_v) {
+    template<typename E>
+    void triShell(std::vector<E> & p_v) {
+        auto gaps = createGaps(p_v.size());
+        for (auto gap : gaps)
+        {
+            for (size_t i = gap; i < p_v.size(); i ++)
+            {
+                E temp = p_v[i];
+                size_t j;
+                for (j = i; j >= gap && p_v[j - gap] > temp; j -= gap)
+                {
+                    std::swap(p_v[j], p_v[j - gap]);
+                }
+                std::swap(p_v[j], p_v[i]);
+            }
+        }
+    }
 
-}
+    std::vector<size_t> createGaps(size_t maximum)
+    {
+        auto gaps;
+        for (size_t k = 1; k <= std::floor(std::log(maximum) / std::log(2)); k++) // log _2 n = log n / log 2 et a ^ (log _a b) = b
+        {
+            gaps.push_back(std::pow(2, k) - 1);
+        }
+        return gaps;
+    }
 
 } // Fin namespace
